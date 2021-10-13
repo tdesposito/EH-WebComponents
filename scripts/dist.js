2a34122771570f29f08a39ad9e318e9ac83b06aa
Todd Esposito <todd@toddesposito.com>
2021-10-13 15:49:26 -0500

    update distribution procedure (more automation)

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,9 +1,16 @@
+const { execSync } = require('child_process')
 const fs = require('fs')
 const glob = require('glob')
 
-const config = require('../package.json')
+function doExec(cmd) {
+  console.log(`Running '${cmd}'...`)
+  execSync(cmd, {stdio: 'inherit'})
+}
 
-if (! fs.existsSync('dist')) { fs.mkdirSync('dist') }
+if (! fs.existsSync('dist')) {
+  fs.mkdirSync('dist')
+}
+
 glob("+(build|bundle)/*.min.js", (err, files) => {
   files.map(async (f) => {
     console.log(`copy ${f} to dist/`)
@@ -11,3 +18,20 @@ glob("+(build|bundle)/*.min.js", (err, files) => {
     fs.copyFileSync(f, target)
   })
 })
+
+// git add and git commit the dist files
+doExec('git add dist/*')
+doExec('git commit -m "update dist files"')
+
+// update and commit the changelog
+doExec('git-changelog . -o CHANGELOG.md')
+doExec('git add CHANGELOG.md')
+doExec('git commit -m "update changelog"')
+doExec('npm version minor')
+doExec('git add package.json')
+
+let pkg = require('./package.json')
+doExec(`git commit -m "bump version to ${pkg.version}"`)
+doExec(`git tag v${pkg.version}`)
+doExec('git push')
+doExec('git push --tags')
